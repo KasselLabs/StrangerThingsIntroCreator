1c678b9dfb69f3f842b4e75dfb15ac3ff83c70dd
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-04-10 21:28:08 +0000

    Deploy strangerthingsintrocreator.kassellabs.io to github.com/KasselLabs/StrangerThingsIntroCreator.git:gh-pages

--- script.js
+++ script.js
@@ -34688,7 +34688,7 @@
 	var requestVideo = function requestVideo(donate, key, email) {
 	  if (email === false) return false;
 
-	  var url = "https://upsidedown.nihey.org/request?code=" + key + "&email=" + email;
+	  var url = "https://upsidedown.kassellabs.io/request?code=" + key + "&email=" + email;
 	  $.ajax({
 	    url: url,
 	    type: 'GET',
@@ -34707,7 +34707,7 @@
 	function downloadVideo(openingKey) {
 	  // check if download is available:
 	  $.ajax({
-	    url: "https://upsidedown.nihey.org/status?code=" + openingKey,
+	    url: "https://upsidedown.kassellabs.io/status?code=" + openingKey,
 	    crossDomain: true,
 	    success: function success(data) {
 	      var queue = data.queue;
