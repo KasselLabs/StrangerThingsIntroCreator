711e65b865c124cab84f06711ace196de1d3944e
BrOrlandi <brorlandi@gmail.com>
2019-10-03 01:54:45 -0300

    Show alert on iOS devices.

--- scripts/verifyBrowser.js
+++ scripts/verifyBrowser.js
@@ -14,6 +14,12 @@ const verifyBrowser = (props) => {
   const canShowBrowserAlert = !props.download && !props.edit;
 
   if(canShowBrowserAlert) {
+    const isIOS = browser.is('ios');
+    if(isIOS){
+      swal("iOS is incompatible", "Sorry but this website doesn't work on iOS devices. For best experiencie try to use it on Chrome on a desktop device.", "error").then(()=>{},()=>{});
+      return;
+    }
+
     const isSafari = browser.is('safari');
     if(isSafari){
       swal("Safari is incompatible", "Sorry but this website may have issues when running in Safari. For best experiencie try to use it on Chrome.", "error").then(()=>{},()=>{});
